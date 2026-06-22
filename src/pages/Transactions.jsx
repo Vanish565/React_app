a8bc407e6c33f774e6aafd7e10ae0195c3e8bf9f
Tyron Wei <tyronwei@gmail.com>
2026-06-22 11:31:25 +0200

    Added a transaction history list gui component

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,37 +1,117 @@
-import { useState } from "react";
-import TransactionForm from "../components/TransactionForm";
-import TransactionList from "../components/TransactionList";
+// import { useState } from "react";
+// import TransactionForm from "../components/TransactionForm";
+// import TransactionList from "../components/TransactionList";
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import { tokens } from "../theme";
+import { mockDataInvoices } from "../data/mockData";
+import Header from "../components/header";
 
 function Transactions() {
-  const [transactions, setTransactions] = useState([]); // states
 
-  const addTransaction = (transaction) => {
-    setTransactions((prev) => [
-      ...prev,
-      {
-        id: crypto.randomUUID(),
-        ...transaction,
-      },
-    ]);
-  };
+  // this is to apply the theme
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  // this is the data format
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cell",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]}>
+          ${params.row.cost}
+        </Typography>
+      ),
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      flex: 1,
+    },
+  ];
+  // old code that needs to be refactored to be a feature later
+  // const [transactions, setTransactions] = useState([]); // states
 
-  const deleteTransaction = (id) => {
-    setTransactions((prev) =>
-      prev.filter((t) => t.id !== id)
-    );
-  };
+  // const addTransaction = (transaction) => {
+  //   setTransactions((prev) => [
+  //     ...prev,
+  //     {
+  //       id: crypto.randomUUID(),
+  //       ...transaction,
+  //     },
+  //   ]);
+  // };
+
+  // const deleteTransaction = (id) => {
+  //   setTransactions((prev) =>
+  //     prev.filter((t) => t.id !== id)
+  //   );
+  // };
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Transactions</h1>
+    // <div style={{ padding: "20px" }}>
+    //   <h1>Transactions</h1>
 
-      <TransactionForm onAddTransaction={addTransaction} />
+    //   <TransactionForm onAddTransaction={addTransaction} />
 
-      <TransactionList
-        transactions={transactions}
-        onDeleteTransaction={deleteTransaction}
-      />
-    </div>
+    //   <TransactionList
+    //     transactions={transactions}
+    //     onDeleteTransaction={deleteTransaction}
+    //   />
+    // </div>
+
+     <Box m="20px">
+      <Header title="Transactions History" subtitle="List of transaction histories" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={mockDataInvoices} columns={columns} />
+      </Box>
+    </Box>
   );
 }
 
